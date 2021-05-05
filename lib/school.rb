86cc1a2f21052a49af22da1cc0e04167c1cc5e43
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-05 10:47:52 +1000

    Tests taken out

--- lib/school.rb
+++ lib/school.rb
@@ -25,28 +25,6 @@ class School
     end
 end
 
-school = School.new("Bayside High School")
-
-school.add_student("Seth Green", 7)
-school.add_student("Meth Green", 7)
-school.add_student("Harry Potter", 8)
-school.add_student("Miriam", 8)
-school.add_student("Josh Potter", 8)
-school.add_student("Haico van der Werf", 8)
-school.add_student("Zach Morris", 9)
-school.add_student("John Smith Morris", 9)
-school.add_student("Christina Walk", 10)
-school.add_student("Anna McRae", 10)
-
-school.roster
-
-puts "Year 7: #{school.grade(7)}"
-puts "Year 8: #{school.grade(8)}"
-puts "Year 9: #{school.grade(9)}"
-puts "Year 10: #{school.grade(10)}"
-
-puts "School sorted: #{school.sort}"
-
 
 
 
